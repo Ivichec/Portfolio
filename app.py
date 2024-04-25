88c7c757fbce1beb50db513b095b2224abd962e9
Ivichec <ivanchecag2003@gmail.com>
2024-04-25 13:59:27 +0200

    error

--- app.py
+++ app.py
@@ -2,13 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
+@app.route("/index")
 def index():
-    return render_template('index.html')
-
-@app.route('/contacto')
-def contacto():
-    return render_template('contacto.html')
+	return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=False)
+	app.run(debug=True)
